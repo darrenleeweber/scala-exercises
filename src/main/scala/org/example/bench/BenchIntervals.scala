b9dfb09234ee142c4c369b29ecd3a0959fe595f1
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-12-29 12:45:52 -0800

    Benchmarks - Intervals with more array lengths

--- src/main/scala/org/example/bench/BenchIntervals.scala
+++ src/main/scala/org/example/bench/BenchIntervals.scala
@@ -5,15 +5,25 @@ import org.openjdk.jmh.infra.Blackhole
 import java.util.concurrent.TimeUnit
 
 @State(Scope.Thread)
-class IntervalsState10() extends GenerateIntervals() {
-  override val arrayLength: Int = 10
-}
-
-@State(Scope.Thread)
 class IntervalsState100() extends GenerateIntervals() {
   override val arrayLength: Int = 100
 }
-
+@State(Scope.Thread)
+class IntervalsState200() extends GenerateIntervals() {
+  override val arrayLength: Int = 200
+}
+@State(Scope.Thread)
+class IntervalsState400() extends GenerateIntervals() {
+  override val arrayLength: Int = 400
+}
+@State(Scope.Thread)
+class IntervalsState600() extends GenerateIntervals() {
+  override val arrayLength: Int = 600
+}
+@State(Scope.Thread)
+class IntervalsState800() extends GenerateIntervals() {
+  override val arrayLength: Int = 800
+}
 @State(Scope.Thread)
 class IntervalsState1000() extends GenerateIntervals() {
   override val arrayLength: Int = 1000
@@ -26,33 +36,28 @@ class IntervalsState1000() extends GenerateIntervals() {
 class BenchIntervals {
 
   @Benchmark
-  def skipped10(state: IntervalsState10, blackhole: Blackhole) : Unit = {
-    blackhole.consume(state.intervals.skipped)
-  }
-
-  @Benchmark
   def skipped100(state: IntervalsState100, blackhole: Blackhole) : Unit = {
     blackhole.consume(state.intervals.skipped)
   }
-
+  @Benchmark
+  def skipped200(state: IntervalsState200, blackhole: Blackhole) : Unit = {
+    blackhole.consume(state.intervals.skipped)
+  }
+  @Benchmark
+  def skipped400(state: IntervalsState400, blackhole: Blackhole) : Unit = {
+    blackhole.consume(state.intervals.skipped)
+  }
+  @Benchmark
+  def skipped600(state: IntervalsState600, blackhole: Blackhole) : Unit = {
+    blackhole.consume(state.intervals.skipped)
+  }
+  @Benchmark
+  def skipped800(state: IntervalsState800, blackhole: Blackhole) : Unit = {
+    blackhole.consume(state.intervals.skipped)
+  }
   @Benchmark
   def skipped1000(state: IntervalsState1000, blackhole: Blackhole) : Unit = {
     blackhole.consume(state.intervals.skipped)
   }
 
-
-  @Benchmark
-  def sorted10(state: IntervalsState10, blackhole: Blackhole) : Unit = {
-    blackhole.consume(state.intervals.sorted)
-  }
-
-  @Benchmark
-  def sorted100(state: IntervalsState100, blackhole: Blackhole) : Unit = {
-    blackhole.consume(state.intervals.sorted)
-  }
-
-  @Benchmark
-  def sorted1000(state: IntervalsState1000, blackhole: Blackhole) : Unit = {
-    blackhole.consume(state.intervals.sorted)
-  }
 }
